8100bacff58874d9011ce8ac9450fa1d220d3848
Jordan <jdkauffman11@gmail.com>
2015-01-28 15:53:29 -0600

    Updated with tree created and scrollview working
    Hard-coded the tree in CreateInitialTree() and fixed the issue with the
    scroll view not working with the tree. Everything works as of this
    commit

--- cs319/Lab2/Lab2Swing.java
+++ cs319/Lab2/Lab2Swing.java
@@ -3,16 +3,22 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.JTabbedPane;
 import javax.swing.JList;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
 
 
 public class Lab2Swing extends JFrame {
@@ -40,50 +46,68 @@ public class Lab2Swing extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "rawtypes" })
 	public Lab2Swing() {
 		
+		//Creates Frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(500,400));
 		setResizable(false);
 		getContentPane().setLayout(null);
-		 
+		
+		//Creates new TabbedPane and sets properties
 		JTabbedPane tabbedPane = new JTabbedPane();
 		tabbedPane.setBounds(0, 0, 500, 378);
 		tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
 		
-		JPanel panel = new JPanel();
-		JScrollPane scroll = new JScrollPane();
-		scroll.setBounds(19, 5, 260, 132);
+		//Creates new JPanel and scroll pane with specific properties
+		JPanel listPanel = new JPanel();
+		JScrollPane listScroll = new JScrollPane();
+		listScroll.setBounds(5,5,470,300);
+		
+		//initializes list, sets it to the scroll pane
 		list = new JList();
-		scroll.setViewportView(list);
         list.setSelectionBackground(Color.yellow);
         list.setSelectionForeground(Color.black);
+		listScroll.setViewportView(list);
 		
+		//Buttons added to add and remove items
 		JButton addButton = new JButton("Add");
 		addButton.setBounds(142, 305, 100, 29);
-		
 		JButton removeButton = new JButton("Remove");
 		removeButton.setBounds(237, 305, 100, 29);
 		
-		scroll.setBounds(5,5,470,300);
+		//Adding properties to the panel
+		listPanel.setLayout(null);
+		listPanel.add(listScroll);
+		listPanel.add(addButton);
+		listPanel.add(removeButton);
+		tabbedPane.addTab("List", null, listPanel, null);
 		
-		panel.setLayout(null);
-		panel.add(scroll);
-		panel.add(addButton);
-		panel.add(removeButton);
-		tabbedPane.addTab("List", null, panel, null);
+		//Creating the tree for the tree tab
+		JPanel treePanel = new JPanel();
+		treePanel.setLayout(null);
 		
+		JScrollPane treeScroll = new JScrollPane();
+		treeScroll.setBounds(5, 5, 470, 275);
 		
-		JTree tree = new JTree();
-		tabbedPane.addTab("Tree", null, tree, null);
+		JTree tree = new JTree(CreateInitialTree());
+		treeScroll.setRowHeaderView(tree);
 		
+		treeScroll.setViewportView(tree);
+		treePanel.add(treeScroll);
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tabbedPane.addTab("Tree", null, treePanel, null);
+		
+		//Table is added but not implemented because table is not used in this lab.
 		tabbedPane.addTab("Table", null);
+		
 		
 		getContentPane().add(tabbedPane);
 		
        
     @SuppressWarnings("rawtypes")
+    //Populates the list
 	DefaultListModel listModel = new DefaultListModel();
 	for(int i = 0; i < dm.getSize(); i++)
 	{
@@ -91,7 +115,7 @@ public class Lab2Swing extends JFrame {
 	}
 	list.setModel(listModel);
         
-	
+	//If add button is clicked the AddBox() method is called.
 	addButton.addActionListener(new ActionListener()
 	{
 		  public void actionPerformed(ActionEvent e)
@@ -101,6 +125,7 @@ public class Lab2Swing extends JFrame {
 		  }
 	});
 	
+	//If Remove button is clicked, item is removed and list is repopulated
 	removeButton.addActionListener(new ActionListener()
 	{
 		public void actionPerformed(ActionEvent e)
@@ -124,61 +149,58 @@ public class Lab2Swing extends JFrame {
 	//Creates the box to add items to the list
 	private void AddBox()
 	{
-		final JFrame frame = new JFrame();
-		frame.setVisible(true);
-		frame.setMinimumSize(new Dimension(400,150));
-		frame.setResizable(false);
-		frame.setTitle("Enter new company name");
-		frame.getContentPane().setLayout(null);
-		
-		final JLabel label = new JLabel();
-		label.setText("What is the new company?");
-		label.setBounds(50, 10, 200, 50);
-		frame.getContentPane().add(label);
-		
-		final JTextField textField = new JTextField();
-		textField.setBounds(45,45,300,25);
-		frame.getContentPane().add(textField);
-		
-		JButton okButton = new JButton("OK");
-		okButton.setBounds(110, 80, 85, 25);
-		frame.getContentPane().add(okButton);
-		okButton.addActionListener(new ActionListener()
+		String company = (String) JOptionPane.showInputDialog(contentPane, 
+				"What is the new company?", "Enter the new company name.", JOptionPane.QUESTION_MESSAGE);
+		dm.AddToList(company);
+	 	@SuppressWarnings("rawtypes")
+		DefaultListModel listModel = new DefaultListModel();
+		for(int i = 0; i < dm.getSize(); i++)
 		{
-			  @SuppressWarnings("unchecked")
-			public void actionPerformed(ActionEvent e)
-			  {
-				  //Not the most efficient way of repopulating the list but I am not familiar enough with the
-				  //libraries to know of a more efficient way.
-				 	dm.AddToList(textField.getText());
-				 	@SuppressWarnings("rawtypes")
-					DefaultListModel listModel = new DefaultListModel();
-					for(int i = 0; i < dm.getSize(); i++)
-					{
-					  listModel.addElement(dm.getElementAt(i));
-					}
-					list.setModel(listModel);
-					frame.dispose();
-			  }
-			});
+		  listModel.addElement(dm.getElementAt(i));
+		}
+		list.setModel(listModel);
 		
-		JButton cancelButton = new JButton("Cancel");
-		cancelButton.setBounds(195, 80, 85, 25);
-		frame.getContentPane().add(cancelButton);
-		cancelButton.addActionListener(new ActionListener()
-		{
-			  public void actionPerformed(ActionEvent e)
-			  {
+
+	}
+	
+	private DefaultMutableTreeNode CreateInitialTree()
+	{
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Animals");
 		
-				 	frame.dispose();
-			  }
-			});
 		
-//		BufferedImage myPicture = ImageIO.read(new File("question.png"));
-//		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-//		picLabel.setBounds(20, 20, 45, 45);
-//		frame.add(picLabel);
+		DefaultMutableTreeNode temp = new DefaultMutableTreeNode("Mammals");
+		root.add(temp);
+		temp.add(new DefaultMutableTreeNode("Human"));
+		temp.add(new DefaultMutableTreeNode("Kangaroo"));
+		temp.add(new DefaultMutableTreeNode("Elephant"));
+		temp.add(new DefaultMutableTreeNode("Goat"));
 		
+		temp = new DefaultMutableTreeNode("Reptiles");
+		root.add(temp);
+		temp.add(new DefaultMutableTreeNode("Lizard"));
+		temp.add(new DefaultMutableTreeNode("Boa"));
+		temp.add(new DefaultMutableTreeNode("Iguana"));
+		
+		temp = new DefaultMutableTreeNode("Birds");
+		root.add(temp);
+		temp.add(new DefaultMutableTreeNode("Duck"));
+		temp.add(new DefaultMutableTreeNode("Pigeon"));
+		temp.add(new DefaultMutableTreeNode("Turkey"));
+		temp.add(new DefaultMutableTreeNode("Goose"));
+		
+		temp = new DefaultMutableTreeNode("Insects");
+		root.add(temp);
+		temp.add(new DefaultMutableTreeNode("Termite"));
+		temp.add(new DefaultMutableTreeNode("Ladybug"));
+		temp.add(new DefaultMutableTreeNode("Fly"));
+		temp.add(new DefaultMutableTreeNode("Ant"));
+		
+		temp = new DefaultMutableTreeNode("Aquatics");
+		root.add(temp);
+		temp.add(new DefaultMutableTreeNode("Swordfish"));
+		temp.add(new DefaultMutableTreeNode("Shark"));
+		temp.add(new DefaultMutableTreeNode("Whale"));
+		return root;
 		
 	}
 }
